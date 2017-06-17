29e470688b8115c6cec36546b02c81c157965450
Larry <larryjamestaylor@gmail.com>
2017-06-16 17:20:43 -0700

    show selected toppings when submitted

--- js/scripts.js
+++ js/scripts.js
@@ -10,50 +10,51 @@ this.veggies = veggies;
 
 var pizza = new Pizza();
 
-// Pizza.prototype.calculate = function(){
-//   var total = 0
-//   if (this.size === "Large") {
-//     total += 16;
-//   } else if (this.size === "Medium") {
-//     total += 13;
-//   } else if (this.size === "Small") {
-//     total += 11;
-//   }
-//   if (this.cheese === "Regular") {
-//     total += 0;
-//   } else if (this.cheese === "Extra") {
-//     total += 1;
-//   }
-//   return total;
-// };
+Pizza.prototype.calculate = function(){
+  var total = 0
+  if (this.size === "Large") {
+    total += 16;
+  } else if (this.size === "Medium") {
+    total += 13;
+  } else if (this.size === "Small") {
+    total += 11;
+  }
+  if (this.cheese === "Regular") {
+    total += 0;
+  } else if (this.cheese === "Extra") {
+    total += 1;
+  }
+  return total;
+};
 
 // UI logic
 $(document).ready(function(){
   $(".pizzaOption").submit(function(event){
   event.preventDefault();
-  console.log("click");
-  $(".placeholder").hide();
-  $("#custName").text(inputtedName);
+  // $""("click");
+  $(".temporary").hide();
+  $("#custName").html(inputtedName);
+  // $(".results").empty();
+  console.log(inputtedName);
+  console.log(inputtedSize);
   $(".results").show();
   $(".totalPrice").show();
-  var inputtedName = $("input#name").val();
+  var inputtedName = $("input#yourName").val();
   var inputtedSize = $(".size").val();
   var inputtedCheese = $(".cheese").val();
   var inputtedSauce = $(".sauce").val();
   var inputtedCrust = $(".crust").val();
   $("input:checkbox[name=meats]:checked").each(function(){
     var meatChoice = $(this).val();
-    $('#meatResult').append(meatChoice + " ");
+    $('#meatResult').append("<li>" + meatChoice + "</li>");
     });
     $("input:checkbox[name=veggies]:checked").each(function(){
       var vegChoice = $(this).val();
-      $('#vegResult').append(vegChoice + " ");
+      $('#vegResult').append("<li>" + vegChoice + "</li>");
       });
 
+  var newPizza = new Pizza(inputtedSize, inputtedCheese, inputtedSauce, inputtedCrust, meatChoice, vegChoice1114);
+
   var total = newPizza.calculate();
-
-  var newPizza = new Pizza(inputtedSize, inputtedCheese, inputtedSauce, inputtedCrust, inputtedMeats, inputtedVeggies);
-
-
   });
 });
